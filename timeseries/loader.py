efb9ea11294b4b65b0b8679ad1e1861a28cbec84
nataliakoliou <natalykoliou@yahoo.gr>
2024-08-05 18:20:30 +0300

    Create training, validation and testing datasets from dataframes

--- timeseries/loader.py
+++ timeseries/loader.py
@@ -1,5 +1,7 @@
+import torch
 import pandas as pd
 import numpy as np
+from torch.utils.data import Dataset
 import os
 
 from . import utils
@@ -17,10 +19,13 @@ def split_data(dir, train_size=57, val_size=1, test_size=1):
     :return: tuple of lists containing npz file paths for train, val, and test sets
     """
     paths = [utils.get_path(dir, filename=file) for file in os.listdir(dir)]
+    logger.debug(f"Found {len(paths)} files in directory: {dir} ready for splitting.")
 
     train_paths = paths[:train_size]
     val_paths = paths[train_size:train_size + val_size]
     test_paths = paths[train_size + val_size:train_size + val_size + test_size]
+
+    logger.debug(f"Splitting complete!")
 
     return (train_paths, val_paths, test_paths)
 
@@ -50,9 +55,11 @@ def combine_data(paths):
     """
     dataframes = []
 
+    logger.debug(f"Combining data from {len(paths)} files.")
+
     for path in paths:
         X, y, _, labels = load_file(path)
-        
+
         num_epochs = X.shape[0]
         samples_per_epoch = X.shape[1]
         label_names = [item[0] for sublist in labels for item in sublist]
@@ -84,16 +91,18 @@ def create_dataframes(train_paths, val_paths, test_paths, exist=False):
     names = ['train', 'val', 'test']
     all_paths = [train_paths, val_paths, test_paths]
 
+    logger.debug("Creating dataframes for training, validation, and testing.")
+
     for paths, name in zip(all_paths, names):
         csv_path = utils.get_path('data', 'csv', filename=f"{name}.csv")
 
         if exist:
             df = pd.read_csv(csv_path)
-            logger.info(f"Loaded existing dataframe from {csv_path}")
+            logger.info(f"Loaded existing dataframe from {csv_path}.")
         else:
             df = combine_data(paths)
             df.to_csv(csv_path, index=False)
-            logger.info(f"Saved new dataframe to {csv_path}")
+            logger.info(f"Saved new dataframe to {csv_path}.")
 
         dataframes.append(df)
 
@@ -101,3 +110,89 @@ def create_dataframes(train_paths, val_paths, test_paths, exist=False):
 
     return tuple(dataframes)
 
+def create_datasets(train_df, val_df, test_df, seq_len=7680):
+    """
+    Create datasets for training, validation, and testing.
+
+    :param train_df: training dataframe
+    :param val_df: validation dataframe
+    :param test_df: testing dataframe
+    :param seq_len: length of the input sequence
+    :return: tuple of datasets
+    """
+
+    datasets = []
+    X, t, y = ["HB_1", "HB_2"], ["Time"], ["Consensus"]
+    dataframes = [train_df, val_df, test_df]
+
+    logger.debug("Creating datasets from dataframes.")
+
+    class TSDataset(Dataset):
+        def __init__(self, df, seq_len, X, t, y, per_epoch=True):
+            """
+            Initializes a time series dataset.
+
+            :param df: dataframe
+            :param seq_len: length of the input sequence
+            :param X: list of feature columns
+            :param t: list of time columns
+            :param y: list of target columns
+            :param per_epoch: whether to create sequences with overlapping epochs or not
+            """
+            self.seq_len = seq_len
+            self.X = pd.concat([df[X], df[t]], axis=1)
+            self.y = df[y]
+            self.per_epoch = per_epoch
+            self.num_samples = self.X.shape[0]
+
+            logger.debug(f"Initializing dataset with seq_len={seq_len}, samples={self.num_samples}, sequences={self.num_seqs}")
+
+        def __len__(self):
+            """
+            :return: length of the dataset
+            """
+            return self.num_seqs
+
+        def __getitem__(self, idx):
+            """
+            Retrieves a sample from the dataset at the specified index.
+
+            :param idx: index of the sample
+            :return: tuple of features and target tensors
+            """
+            if self.per_epoch:
+                start_idx = idx * self.seq_len
+            else:
+                start_idx = idx
+
+            end_idx = start_idx + self.seq_len
+
+            X = self.X.iloc[start_idx:end_idx].values
+            y = self.y.iloc[start_idx:end_idx].values
+
+            return torch.FloatTensor(X), torch.FloatTensor(y)
+        
+        @property
+        def max_seq_id(self):
+            """
+            :return: maximum index for a sequence
+            """
+            return self.num_samples - self.seq_len
+        
+        @property
+        def num_seqs(self):
+            """
+            :return: number of sequences that can be created from the dataset
+            """
+            if self.per_epoch:
+                return self.max_seq_id + 1
+            else:
+                return self.num_samples // self.seq_len 
+
+    for df in dataframes:
+        dataset = TSDataset(df, seq_len, X, t, y)
+        datasets.append(dataset)
+
+        logger.info(f"Dataset created with {len(dataset)} sequences!")
+
+    return tuple(datasets)
--- timeseries/train.py
+++ timeseries/train.py
@@ -1,14 +1,13 @@
 from . import utils
 from . import loader
 
-dfs_exist = False
-
 def main():
     npz_dir = utils.get_dir('data', 'npz')
 
-    data = loader.split_data(dir=npz_dir, train_size=57, val_size=1, test_size=1)
+    datapaths = loader.split_data(dir=npz_dir, train_size=2, val_size=1, test_size=1)
     
-    train_df, val_df, test_df = loader.create_dataframes(*data, exist=False)
+    dataframes = loader.create_dataframes(*datapaths, exist=True)
+    datasets = loader.create_datasets(*dataframes, seq_len=7680)
 
 if __name__ == "__main__":
     main()
