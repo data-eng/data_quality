e222a139618d6882d71cfc5ee657fe04315c4a99
nataliakoliou <natalykoliou@yahoo.gr>
2024-08-07 15:58:31 +0300

    Adjust project structure to introduce autoencoder

---
+++ timeseries/transformer/model.py
@@ -0,0 +1,214 @@
+import math
+import torch
+import torch.nn as nn
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model, num_heads):
+        """
+        Initialize the Multi-Head Attention module.
+
+        :param d_model: dimension of the input and output features
+        :param num_heads: number of attention heads
+
+        The module computes attention weights with shape torch.Size([batch_size, num_heads, seq_length, seq_length]), 
+        which can be accessed as model.encoder[layer_id].self_attn.attn_weights.
+        """
+        super(MultiHeadAttention, self).__init__()
+
+        assert d_model % num_heads == 0, "d_model must be divisible by num_heads"
+        
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.d_k = d_model // num_heads
+        
+        self.W_q = nn.Linear(d_model, d_model)
+        self.W_k = nn.Linear(d_model, d_model)
+        self.W_v = nn.Linear(d_model, d_model)
+        self.W_o = nn.Linear(d_model, d_model)
+
+        self.attn_weights = None
+        
+    def attention_scores(self, Q, K, V):
+        """
+        Calculates the attention scores and applies them to the values.
+        
+        :param Q: query matrix
+        :param K: key matrix
+        :param V: value matrix
+        :return: attention scores per head
+        """
+        dot_product = torch.matmul(Q, K.transpose(-2, -1))
+
+        scaling_factor = math.sqrt(self.d_k)
+        attn_scores = dot_product / scaling_factor
+
+        attn_probs = torch.softmax(attn_scores, dim=-1)
+        self.attn_weights = attn_probs
+
+        attn_scores = torch.matmul(attn_probs, V)
+
+        return attn_scores
+        
+    def split_heads(self, x):
+        """
+        Split the input into multiple heads.
+
+        :param x: tensor (batch_size, seq_length, d_model)
+        :return: tensor (batch_size, num_heads, seq_length, d_k)
+        """
+        batch_size, seq_length, _ = x.size()
+
+        x = x.view(batch_size, seq_length, self.num_heads, self.d_k)
+        x = x.transpose(1, 2)
+        
+        return x
+        
+    def combine_heads(self, x):
+        """
+        Combine multiple heads into a single tensor.
+
+        :param x: tensor (batch_size, num_heads, seq_length, d_k)
+        :return: tensor (batch_size, seq_length, d_model)
+        """
+        batch_size, _, seq_length, _ = x.size()
+
+        x = x.transpose(1, 2).contiguous()
+        x = x.view(batch_size, seq_length, self.d_model)
+        
+        return x
+        
+    def forward(self, Q, K, V):
+        """
+        Forward pass for multi-head attention.
+
+        :param Q: query matrix
+        :param K: key matrix
+        :param V: value matrix
+        :return: multi-head attention matrix
+        """
+        Q = self.split_heads(x=self.W_q(Q))
+        K = self.split_heads(x=self.W_k(K))
+        V = self.split_heads(x=self.W_v(V))
+        
+        attn_scores = self.attention_scores(Q, K, V)
+        attn_matrix = self.combine_heads(attn_scores)
+        
+        output = self.W_o(attn_matrix)
+
+        return output
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model, dim_feedforward):
+        """
+        Initialize the FeedForward module.
+
+        :param d_model: dimension of the input and output features
+        :param dim_feedforward: dimension of the feedforward network hidden layer
+        """
+        super(FeedForward, self).__init__()
+
+        self.fc1 = nn.Linear(d_model, dim_feedforward)
+        self.fc2 = nn.Linear(dim_feedforward, d_model)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        """
+        Forward pass through the feedforward network.
+
+        :param x: tensor (batch_size, seq_length, d_model)
+        :return: tensor (batch_size, seq_length, d_model)
+        """
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+
+        return x
+    
+class Encoder(nn.Module):
+    def __init__(self, d_model, num_heads, dim_feedforward, dropout=0.1):
+        """
+        Initialize the Encoder module.
+
+        :param d_model: dimension of the input and output features
+        :param num_heads: number of attention heads
+        :param dim_feedforward: dimension of the feedforward network hidden layer
+        :param dropout: rate for randomly deactivating neurons during training
+        """
+        super(Encoder, self).__init__()
+
+        self.self_attn = MultiHeadAttention(d_model, num_heads)
+
+        self.ffn = FeedForward(d_model, dim_feedforward)
+
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, x):
+        """
+        Forward pass through the encoder.
+
+        :param x: tensor (batch_size, seq_length, d_model)
+        :return: tensor (batch_size, seq_length, d_model)
+        """
+        self_attn_x = self.self_attn(x, x, x)
+
+        x = x + self.dropout(self_attn_x)
+        x = self.norm1(x)
+        
+        ffn_x = self.ffn(x)
+
+        x = x + self.dropout(ffn_x)
+        x = self.norm2(x)
+        
+        return x
+    
+class Transformer(nn.Module):
+    def __init__(self, in_size=3, out_size=5, d_model=512, num_heads=1, num_layers=1, dim_feedforward=2048, dropout=0):
+        """
+        Initialize the Transformer model.
+
+        :param in_size: size of the input features
+        :param out_size: size of the output classes
+        :param nhead: number of attention heads
+        :param num_layers: number of encoder layers
+        :param dim_feedforward: dimension of the feedforward network hidden layer
+        :param dropout: dropout rate
+        """
+        super(Transformer, self).__init__()
+
+        self.embedding = nn.Linear(in_size, d_model)
+
+        self.encoder = nn.ModuleList([Encoder(d_model, num_heads, dim_feedforward, dropout) for _ in range(num_layers)])
+        
+        self.classifier = nn.Linear(d_model, out_size)
+        self.dropout = nn.Dropout(dropout)
+
+        self.init_weights()
+
+    def init_weights(self):
+        """
+        Initialize the weights and biases of the classifier linear layer:
+        - Set the bias of the classifier linear layer to zero.
+        - Initialize the weights with values drawn from a Xavier uniform distribution.
+        """
+        self.classifier.bias.data.zero_()
+        nn.init.xavier_uniform_(self.classifier.weight.data)
+        
+    def forward(self, x):
+        """
+        Forward pass of the transformer model.
+
+        :param x: tensor (batch_size, seq_length, in_size)
+        :return: tensor (batch_size, seq_length, out_size)
+        """
+        x = self.embedding(x)
+        x = self.dropout(x)
+                                             
+        for enc_layer in self.encoder:
+            x = enc_layer(x=x)
+
+        output = self.classifier(x)
+
+        return output
---
+++ timeseries/transformer/train.py
@@ -0,0 +1,177 @@
+import matplotlib.pyplot as plt
+
+from . import utils
+from .loader import *
+from .model import *
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+logger.info(f'Device is {device}')
+
+def train(data, classes, epochs, patience, lr, criterion, model, optimizer, scheduler, visualize=False):
+    model.to(device)
+
+    train_data, val_data = data
+
+    batches = len(train_data)
+    num_classes = len(classes)
+
+    optimizer = utils.get_optim(optimizer, model, lr)
+    scheduler = utils.get_sched(*scheduler, optimizer)
+
+    best_val_loss = float('inf')
+    stationary = 0
+    train_losses, val_losses = [], []
+
+    checkpoints = {'epochs': 0, 
+                   'best_epoch': 0, 
+                   'best_train_loss': float('inf'), 
+                   'best_val_loss': float('inf'), 
+                   'precision_micro': 0, 
+                   'precision_macro': 0, 
+                   'precision_weighted': 0,
+                   'recall_micro': 0, 
+                   'recall_macro': 0, 
+                   'recall_weighted': 0, 
+                   'fscore_micro': 0,
+                   'fscore_macro': 0, 
+                   'fscore_weighted': 0}
+
+    for epoch in range(epochs):
+        model.train()
+
+        total_train_loss = 0.0
+        true_values, pred_values = [], []
+
+        for _, (X, y) in enumerate(train_data):
+            X, y = X.to(device), y.to(device)
+            y_pred = model(X)
+
+            batch_size, seq_len, _ = y_pred.size()
+
+            y_pred = y_pred.reshape(batch_size * seq_len, num_classes)
+            y = y.reshape(batch_size * seq_len)
+            
+            train_loss = criterion(pred=y_pred, true=y)
+
+            optimizer.zero_grad()
+            train_loss.backward()
+            optimizer.step()
+
+            total_train_loss += train_loss.item()
+
+            true_values.append(y.cpu().numpy())
+            pred_values.append(y_pred.detach().cpu().numpy())
+
+        avg_train_loss = total_train_loss / batches
+        train_losses.append(avg_train_loss)
+
+        model.eval()
+        total_val_loss = 0.0
+
+        with torch.no_grad():
+            for X, y in val_data:
+                X, y = X.to(device), y.to(device)
+                y_pred = model(X)
+
+                batch_size, seq_len, _ = y_pred.size()
+
+                y_pred = y_pred.reshape(batch_size * seq_len, num_classes)
+                y = y.reshape(batch_size * seq_len)
+
+                val_loss = criterion(pred=y_pred, true=y)
+                total_val_loss += val_loss.item()
+        
+        avg_val_loss = total_val_loss / batches
+        val_losses.append(avg_val_loss)
+
+        true_values = np.concatenate(true_values)
+        pred_values = np.concatenate(pred_values)
+
+        true_classes = true_values.tolist()
+        pred_classes = [utils.get_max(pred).index for pred in pred_values]
+        
+        logger.info(f"Epoch [{epoch + 1}/{epochs}], Training Loss: {avg_train_loss:.6f}, Validation Loss: {avg_val_loss:.6f}")
+
+        if avg_val_loss < best_val_loss:
+            best_val_loss = avg_val_loss
+            stationary = 0
+
+            logger.info(f"New best val found! ~ Epoch [{epoch + 1}/{epochs}], Val Loss {avg_val_loss}")
+
+            path = utils.get_path("models", filename="transformer.pth")
+            torch.save(model.state_dict(), path)
+
+            checkpoints.update({'best_epoch': epoch+1, 
+                                'best_train_loss': avg_train_loss, 
+                                'best_val_loss': best_val_loss, 
+                                **utils.get_prfs(true=true_classes, pred=pred_classes)})
+
+            if visualize:
+                utils.visualize(type="heatmap",
+                        values=(true_classes, pred_classes), 
+                        labels=("True Values", "Predicted Values"), 
+                        title="Train Heatmap ",
+                        classes=classes,
+                        coloring=['azure', 'darkblue'],
+                        path=utils.get_dirs('static'))
+                
+        else:
+            stationary += 1
+
+        if stationary >= patience:
+            logger.info(f"Early stopping after {epoch + 1} epochs without improvement. Patience is {patience}.")
+            break
+
+        scheduler.step()
+
+    cfn = utils.get_path("static", filename="train_checkpoints.json")
+    checkpoints.update({'epochs': epoch+1})
+    utils.save_json(data=checkpoints, filename=cfn)
+    
+    if visualize:
+        utils.visualize(type="multi-plot",
+                        values=[(range(1, len(train_losses) + 1), train_losses), (range(1, len(val_losses) + 1), val_losses)], 
+                        labels=("Epoch", "Loss"), 
+                        title="Loss Curves",
+                        plot_func=plt.plot,
+                        coloring=['brown', 'royalblue'],
+                        names=["Training", "Validation"],
+                        path=utils.get_dirs('static'))
+
+    logger.info(f'\nTraining complete!\nFinal Training Loss: {avg_train_loss:.6f} & Validation Loss: {best_val_loss:.6f}\n')
+
+def main():
+    classes = ['W','R','N1','N2','N3']
+    npz_dir = utils.get_dir('..', 'data', 'npz')
+    seq_len = 7680 // 1
+
+    datapaths = split_data(dir=npz_dir, train_size=2, val_size=1, test_size=1)
+    
+    train_df, val_df, _ = create_dataframes(datapaths, exist=True)
+    weights = extract_weights(df=train_df, label_col='Consensus')
+
+    datasets = create_datasets(dataframes=(train_df, val_df), seq_len=seq_len)
+
+    dataloaders = create_dataloaders(datasets, batch_size=256)
+
+    model = Transformer(in_size=3,
+                        out_size=len(classes),
+                        d_model=64,
+                        num_heads=1,
+                        num_layers=2,
+                        dim_feedforward=2048,
+                        dropout=0)
+    
+    train(data=dataloaders,
+          classes=classes,
+          epochs=2,
+          patience=30,
+          lr=5e-4,
+          criterion=utils.WeightedCrossEntropyLoss(weights),
+          model=model,
+          optimizer="AdamW",
+          scheduler=("StepLR", 1.0, 0.98),
+          visualize=True)
+
+if __name__ == "__main__":
+    main()
